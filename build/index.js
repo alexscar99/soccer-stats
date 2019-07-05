8dd136ee9a2fc4a60af4d5b885572abd5905988e
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-05 19:47:33 -0400

    Brought in data in CSV format, created enum for match results, started basic analysis

--- build/index.js
+++ build/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log('testing');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('soccer.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var tottenhamWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Tottenham' && match[5] === MatchResult.HomeWin) {
+        tottenhamWins++;
+    }
+    else if (match[2] === 'Tottenham' && match[5] === MatchResult.AwayWin) {
+        tottenhamWins++;
+    }
+}
+console.log("Tottenham won " + tottenhamWins + " games this past season");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('testing');
+import fs from 'fs';
+
+const matches = fs
+    .readFileSync('soccer.csv', {
+        encoding: 'utf-8'
+    })
+    .split('\n')
+    .map(
+        (row: string): string[] => {
+            return row.split(',');
+        }
+    );
+
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+let tottenhamWins = 0;
+
+for (let match of matches) {
+    if (match[1] === 'Tottenham' && match[5] === MatchResult.HomeWin) {
+        tottenhamWins++;
+    } else if (match[2] === 'Tottenham' && match[5] === MatchResult.AwayWin) {
+        tottenhamWins++;
+    }
+}
+
+console.log(`Tottenham won ${tottenhamWins} games this past season`);
