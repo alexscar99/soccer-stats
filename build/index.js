942edd720e38534872b16a2bd304feb5d88f82c3
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-07 13:18:15 -0400

    Second refactor: using composition and an interface, saved first refactor in 'inheritance' folder

--- build/index.js
+++ build/index.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
+var CsvFileReader_1 = require("./CsvFileReader");
 var MatchResult_1 = require("./MatchResult");
-var reader = new MatchReader_1.MatchReader('soccer.csv');
-reader.read();
+// create an object that satisfies the 'DataReader' interface
+var csvFileReader = new CsvFileReader_1.CsvFileReader('soccer.csv');
+// create an instance of 'MatchReader' and pass in something satisfying
+// the 'DataReader' interface
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 var tottenhamWins = 0;
-for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
     var match = _a[_i];
     if (match[1] === 'Tottenham' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         tottenhamWins++;
@@ -14,4 +19,4 @@ for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
         tottenhamWins++;
     }
 }
-console.log("Tottenham won " + tottenhamWins + " games this past season");
+console.log("Tottenham won " + tottenhamWins + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('soccer.csv');
-reader.read();
+// create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('soccer.csv');
+
+// create an instance of 'MatchReader' and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let tottenhamWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Tottenham' && match[5] === MatchResult.HomeWin) {
         tottenhamWins++;
     } else if (match[2] === 'Tottenham' && match[5] === MatchResult.AwayWin) {
@@ -14,4 +20,4 @@ for (let match of reader.data) {
     }
 }
 
-console.log(`Tottenham won ${tottenhamWins} games this past season`);
+console.log(`Tottenham won ${tottenhamWins} games`);
