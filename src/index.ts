32ce91e1115d6790a1a48225ddfbd4d2bcaa2e5b
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-07 18:13:00 -0400

    Went with 2nd approach of composition based.
    Created Summary class and two interfaces for Analyzers and OutputTargets
    
    First analyzer of WinsAnalysis made, moved logic from index.ts to this more appropriate class

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
 
 // create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('soccer.csv');
 
-// create an instance of 'MatchReader' and pass in something satisfying
-// the 'DataReader' interface
+// create an instance of 'MatchReader' and pass in something
+// satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let tottenhamWins = 0;
-
-for (let match of matchReader.matches) {
-    if (match[1] === 'Tottenham' && match[5] === MatchResult.HomeWin) {
-        tottenhamWins++;
-    } else if (match[2] === 'Tottenham' && match[5] === MatchResult.AwayWin) {
-        tottenhamWins++;
-    }
-}
-
-console.log(`Tottenham won ${tottenhamWins} games`);
