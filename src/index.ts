4d60666b908bb7eed9440fc6ee7937a24dcc274d
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-07 19:01:33 -0400

    Refactored Summary and MatchReader to use static methods for creating a wins analysis HTML report and reading a CSV respectively. Cleaned up index.ts to reflect these changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('soccer.csv');
+const matchReader = MatchReader.fromCsv('soccer.csv');
+const summary = Summary.winsAnalysisHtmlReport('Tottenham');
 
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(new WinsAnalysis('Tottenham'), new HtmlReport());
-
 summary.buildAndPrintReport(matchReader.matches);
